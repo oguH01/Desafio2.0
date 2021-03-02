acba397cb01dbf164cbaa3288d266767bc35c91a
Hugo Grande <77947730+oguH01@users.noreply.github.com>
2021-03-02 15:47:20 -0300

    Update desafio2.js

--- templates/android-dot-com/desafio2.js
+++ templates/android-dot-com/desafio2.js
@@ -3,6 +3,7 @@
 //CAPTURANDO O ELEMENTO HTML USANDO UM ATRIBUTO AO INVÉS DA CLASSE PARA PRATICAR O USO DO DATA ATTRIBUTE:
 var viewSource = document.querySelector("[teste]");
 var botaoViado = document.querySelector(".Viado");
+var androidTitulo = document.querySelector(".android-logo-image");
 
 //ADICIONANDO UM ESCUTADOR NO ELEMENTO DO BOTÃO VIEW SOURCE, QUE AO SER CLICADO IRÁ COLOCAR A VARIÁVEL E O EVENTO NO DATALAYER:
 viewSource.addEventListener("click", function(){
@@ -17,4 +18,8 @@ viewSource.addEventListener("click", function(){
 
 botaoViado.addEventListener("click", function(){
   dataLayer.push({});
-    
+;});
+  
+androidTítulo.addEventListener("click", function(){
+  dataLayer.push({});
+ });
